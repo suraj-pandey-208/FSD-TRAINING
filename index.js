858bad3f75526dcda8ffa1ebd2151e45e486f234
Suraj Pandey <surajpandey0289@gmail.com>
2026-07-27 15:36:59 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello using js")
+// function squareRoot(a,b){
+//     return Math.sqrt(a)+Math.sqrt(b)
+// }
+
+
+// console.log(squareRoot(25,16))
+
+let a=34
+if(a>20){
+    let a=45;
+    console.log(+a)
+}
+console.log(+a)
